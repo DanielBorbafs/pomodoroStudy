597b9a9b44a088baaf23ff7e7885c3d5f74b4316
DanielBorbafs <dev.danielborba@gmail.com>
2024-09-03 23:56:14 -0300

    chore: css modules

--- studies/src/components/Botao/index.tsx
+++ studies/src/components/Botao/index.tsx
@@ -1,11 +1,12 @@
 // Class components
 
 import React from "react";
+import style from './Botao.module.scss'
 
 class Botao extends React.Component{
     render() {
         return (
-            <button>
+            <button className={style.botao}>
                 Botão
             </button>
         )
